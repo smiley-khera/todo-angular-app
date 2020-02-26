221e122b28a87f0d982a85f2e6dfca74ffaea597
smiley <skhera@watermarkinsights.com>
2020-02-26 14:56:03 +0530

    Delete tag

--- src/controllers/tags_controller/index.js
+++ src/controllers/tags_controller/index.js
@@ -1,6 +1,22 @@
-todoApp.controller('tagsIndexCtrl', ['$scope','$location','$log', '$http', function ($scope, $location, $log, $http) {
-    $http.get('http://localhost:3001/api/v1/tags.json?page=1')
-        .success(function (result) {
-            $scope.tags = result
-        })
+todoApp.controller('tagsIndexCtrl', ['$scope', '$location', '$log', '$http', function ($scope, $location, $log, $http) {
+
+    /*Function to fetch all tags*/
+    $scope.fetchTags = function () {
+        $http.get('http://localhost:3001/api/v1/tags.json?page=1')
+            .success(function (result) {
+                $scope.tags = result
+            })
+    };
+
+    /*Function call to fetch tags*/
+    $scope.fetchTags();
+
+    /*Function to delete tag*/
+    $scope.deleteTag = function(tag) {
+        $http.delete('http://localhost:3001/api/v1/tags/' + tag + '.json')
+            .success(function (result) {
+                let deletedTag = $scope.tags.findIndex(item => item.tag.id.$oid === tag);
+                $scope.tags.splice(deletedTag, 1);
+            })
+    }
 }]);
